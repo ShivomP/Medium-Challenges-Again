76658ce50aeb18426056ec507e3f2afe11a6094b
Shivom <shivompaudel@icloud.com>
2022-09-02 17:58:01 -0500

    third times the charm

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Question 1
 
 function returnFalsy (one, two){
-    return (!one ? one : two)
+   return (!one ? one : two)
 }
 
-console.log(returnFalsy(undefined, 500))
+console.log(returnFalsy(false, 500))
 
 // Question 2
 
@@ -17,7 +17,7 @@ console.log(arrLength([1, 2, 3, 4, 5]))
 // Question 3
 
 function lastElement (arr) {
-    return arr [arr.length - 1]
+    return arr[arr.length - 1]
 }
 
 console.log(lastElement([1, 2, 3, 62]))
@@ -26,7 +26,7 @@ console.log(lastElement([1, 2, 3, 62]))
 
 function arrSum(arr){
     let sum = 0
-    for (let i = 0; i < arr.length; ++i){
+    for(let i = 0; i < arr.length; i++){
         sum = sum + arr[i]
     }
     return sum
@@ -38,7 +38,7 @@ console.log(arrSum([2, 2, 2]))
 
 function progressiveSum(num){
     let total = 0
-    for (let i = 0; i <= num; ++i){
+    for(let i = 0; i <= num; i++){
         total = total + i
     }
     return total
@@ -49,35 +49,34 @@ console.log(progressiveSum(4))
 // Question 6
 
 function calcTime(time){
-    let minutes = Math.floor (time / 60)
-    let seconds = time % 60
-    if (minutes.toString().length === 1){
+   let minutes = Math.floor (time / 60)
+   let seconds = time % 60
+   if (minutes.toString().length === 1)
         minutes = '0' + minutes
-    }
-    return minutes + ':' + seconds
+   return minutes + ':' + seconds
 }
 
-console.log(calcTime(75))
+console.log(calcTime(90))
 
 // Question 7
 
 function getMax(arr){
-    let max = arr[0]
-    for (let i = 0; i <= arr.length; ++i){
-        if (arr[i] > max){
-            max = arr[i]
-        }
+   let max = arr[0]
+   for(let i = 0; i <= arr.length; i++){
+    if(max < arr[i]){
+        max = arr[i]
     }
-    return max
+   }
+   return max
 }
 
-console.log(getMax([-12, 10, -20]))
+console.log(getMax([-12, 10, 2000]))
 
 // Question 8
 
 function reverseString(str){
     let reversedString = ''
-    for (let i = str.length - 1; i >= 0; --i){
+    for(let i = str.length - 1; i >= 0; --i){
         reversedString += str[i]
     }
     return reversedString
@@ -88,24 +87,42 @@ console.log(reverseString('shivom'))
 // Question 9
 
 function convertToZero(arr){
+    let newArr= []
     for(let i = 0; i < arr.length; i++){
-        arr[i] = 0
+        newArr[i] = 0
     }
-    return arr
+    return newArr
 }
 
 console.log(convertToZero([1, 2, 3, 4, 5]))
+
+function zero(arr){
+    return arr.fill(0)
+}
+
+console.log(zero([1, 2, 3, 4, 5]))
+
+function shivom(arr){
+    return arr.map(elem => 0)
+}
+
+console.log(shivom([1, 2, 3, 4, 5]))
 
 // Question 10
 
 function removeApples(arr){
     let noApples = []
-    for (let i = 0; i < arr.length; ++i){
-        if (arr[i] !== 'apples'){
-            noApples.push(arr[i])
-        }
-    }
-    return noApples
+   for(let i = 0; i < arr.length; ++i){
+    if (arr[i] !== 'apples')
+        noApples.push(arr[i])
+   }
+   return noApples
+}
+
+console.log(removeApples(['apples', 'banana', 'oranges']))
+
+function fruit(arr){
+    return arr.filter(elem => elem !== 'apples')
 }
 
 console.log(removeApples(['apples', 'banana', 'oranges']))
@@ -113,20 +130,25 @@ console.log(removeApples(['apples', 'banana', 'oranges']))
 // Question 11
 
 function filterOutFlasy(arr){
-    let noFalsy = []
-    for(let i = 0; i < arr.length; ++i){
-        if(!!arr[i] === true){
-            noFalsy.push(arr[i])
-        }
+   let noFlasy = []
+   for(let i = 0; i < arr.length; i++){
+    if(!!arr[i] === true){
+        noFlasy.push(arr[i])
     }
-    return noFalsy
+   }
+   return noFlasy
 }
 
 console.log(filterOutFlasy([0, null, undefined, 'apples', 'shivom']))
 
+function kive(arr){
+    return arr.filter(elem => !!elem === true)
+}
+
+console.log(kive([0, null, undefined, 'apples', 'shivom']))
 
 function convertToBool(arr){
-    return arr.map(elem => !!elem) 
+    return arr.map(elem => !!elem)
 }
 
 console.log(convertToBool([1000, 0, 'shiv', undefined]))
